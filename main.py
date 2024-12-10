1a27ebfebba4c1ce9505265606191fec0be4d09e
lochinbekabdullaev <alochinbek05@gmail.com>
2024-12-09 22:45:36 -0500

    Update main.py
    Removed the celsius and kph info since we use fahrenheit and mph in the usa

--- main.py
+++ main.py
@@ -30,19 +30,17 @@ async def weather(ctx: commands.Context, *, city):
             data = await res.json()
 
             location = data["location"]["name"]
-            temp_c = data["current"]["temp_c"]
             temp_f = data["current"]["temp_f"]
             humidity = data["current"]["humidity"]
-            wind_kph = data["current"]["wind_kph"]
             wind_mph = data["current"]["wind_mph"]
             condition = data["current"]["condition"]["text"]
             image_url = "http:" + data["current"]["condition"]["icon"]
 
             embed = nextcord.Embed(title=f"Weather for {location}", description=f"The condition in `{location}` is ` "
                                                                                 f"{condition}`")
-            embed.add_field(name="Temperature", value=f"C: {temp_c} | F: {temp_f}")
+            embed.add_field(name="Temperature", value=f"F: {temp_f}")
             embed.add_field(name="Humidity", value=f"{humidity}")
-            embed.add_field(name="Wind Speeds", value=f"KPH: {wind_kph} | MPH: {wind_mph}")
+            embed.add_field(name="Wind Speeds", value=f"MPH: {wind_mph}")
             embed.set_thumbnail(url=image_url)
 
             await ctx.send(embed=embed)
